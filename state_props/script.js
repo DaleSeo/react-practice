7b7136bbf012c029ab48bdd0d8323b765d325fdb
Dale Seo <dale.seo@gmail.com>
2016-09-21 23:16:57 +0900

    add event handler

--- state_props/script.js
+++ state_props/script.js
@@ -1,16 +1,53 @@
-var Parent = React.createClass({
-  getInitialState: function() {
-    return { name: 'Frarthur' }
-  },
-  render: function () {
-    return <Child name={this.state.name} />;
-  }
-});
-
 var Child = React.createClass({
+  handleChange: function(e) {
+    var name = e.target.value;
+    this.props.onChange(name);
+  },
+
   render: function () {
-    return <h1>Hey, my name is {this.props.name}!</h1>;
+    return (
+        <div>
+          <h1>
+            Hey my name is {this.props.name}!
+          </h1>
+          <select id="great-names" onChange={this.handleChange}>
+            <option value="Frarthur">
+              Frarthur
+            </option>
+
+            <option value="Gromulus">
+              Gromulus
+            </option>
+
+            <option value="Thinkpiece">
+              Thinkpiece
+            </option>
+          </select>
+        </div>
+    );
   }
 });
 
-ReactDOM.render(<Parent />, document.getElementById('app'));
+var Parent = React.createClass({
+  getInitialState: function () {
+    return { name: 'Frarthur' };
+  },
+
+  changeName: function(newName) {
+    this.setState({
+      name: newName
+    });
+  },
+
+  render: function () {
+    return (
+        <Child
+            name={this.state.name} onChange={this.changeName}/>
+    );
+  }
+});
+
+ReactDOM.render(
+    <Parent />,
+    document.getElementById('app')
+);
